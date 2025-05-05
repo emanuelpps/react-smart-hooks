6997db8c928acb4adba041feb3d82b78d91298d6
emanuelpps <emanuelpages.ps@gmail.com>
2025-05-05 15:58:25 -0300

    chore: files configurations

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,17 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "react-smart-hooks",
+      formats: ["es", "umd"],
     },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output:{
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
   },
   plugins: [dts({ outDir: "dist" })],
 });
