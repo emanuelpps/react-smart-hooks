b4c77d15339a93a33e2e493223bbcaf9aea00de5
emanuelpps <emanuelpages.ps@gmail.com>
2025-05-05 16:25:39 -0300

    chore: added files configurations

--- eslint.config.js
+++ eslint.config.js
@@ -3,16 +3,28 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default defineConfig([
- { files: ["**/*.{js,mjs,cjs,ts}"] },
- { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
- { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
- tseslint.configs.recommended,
- {
-   rules: {
-     'no-var': 'error',
-     'no-console': 'warn'
-   }
- }
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "@typescript-eslint/no-explicity-any": "error",
+      "no-console": "error",
+      "no-unused-vars": [
+        "error",
+        { vars: "all", args: "after-used", ignoreRestSiblings: false },
+      ],
+    },
+  },
 ]);
